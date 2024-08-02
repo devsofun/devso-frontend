66ffd80b85f4da312e58cbaa6e3a4f6fe18e2fba
RegirX <regirx@outlook.com>
2024-08-02 06:45:18 +0000

    chore: 合并 Layout.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <Layout />
+  <el-container class="h-screen max-w-screen flex flex-col overflow-x-hidden">
+    <div class="flex flex-grow">
+      <router-view class="min-h-full min-w-full"></router-view>
+    </div>
+    <div class="footer h-15">
+      <div class="text-center text-gray-500">
+        Powered by DevSo.Fun
+      </div>
+    </div>
+  </el-container>
 </template>
 
 <script setup lang="ts">
-import { defineComponent } from 'vue';
-import Layout from './components/Layout.vue';
-
-defineComponent({
-  name: 'App',
-});
 </script>
+
+<style scoped>
+.footer {
+  @apply text-center p-4 bg-gray-100 border-t border-gray-200;
+}
+</style>
